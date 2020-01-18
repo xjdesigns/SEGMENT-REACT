da736946f89fe7eb7b1cb2dd934ad8566ae7523e
Jason Jacobson <jdesigns.jason@gmail.com>
2020-01-18 07:35:19 -0600

    Updates

---
+++ precache-manifest.1061352b4c58ca30380c9aff2509ad38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43f847da6bf36815354e7899817a00c",
+    "url": "/Segment-REACT/index.html"
+  },
+  {
+    "revision": "a9a21556b784ce56106d",
+    "url": "/Segment-REACT/static/css/main.334002ca.chunk.css"
+  },
+  {
+    "revision": "2e76a9646a89817ba9a2",
+    "url": "/Segment-REACT/static/js/2.629e04db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Segment-REACT/static/js/2.629e04db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9a21556b784ce56106d",
+    "url": "/Segment-REACT/static/js/main.029982bc.chunk.js"
+  },
+  {
+    "revision": "e668d5d3e21270630f31",
+    "url": "/Segment-REACT/static/js/runtime-main.e4868476.js"
+  }
+]);
